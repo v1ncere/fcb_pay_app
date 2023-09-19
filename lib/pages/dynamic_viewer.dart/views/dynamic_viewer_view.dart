0edea064f985bd3ea1649c1098835f0c604cedd7
Vincent <hiei.grepo@gmail.com>
2023-09-19 21:42:01 +0800

    FROM WFH 19/09/2023

--- lib/pages/dynamic_viewer.dart/views/dynamic_viewer_view.dart
+++ lib/pages/dynamic_viewer.dart/views/dynamic_viewer_view.dart
@@ -1,16 +1,36 @@
+import 'package:fcb_pay_app/pages/dynamic_viewer.dart/dynamic_viewer.dart';
+import 'package:fcb_pay_app/utils/utils.dart';
 import 'package:fcb_pay_app/widgets/widgets.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DynamicViewerView extends StatelessWidget {
   const DynamicViewerView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const CustomCardContainer(
-      color: Color(0xFFFFFFFF),
-      children: [
-        
-      ],
+    return BlocBuilder<WidgetsBloc, WidgetsState>(
+      builder: (context, state) {
+        if (state.status.isLoading) {
+          return const Center(child: CircularProgressIndicator());
+        } 
+        if (state.status.isSuccess) {
+          return const CustomCardContainer(
+            color: Color(0xFFFFFFFF),
+            children: [
+
+            ],
+          );
+        }
+        if (state.status.isError) {
+          return Center(
+            child: Text('${state.errorMsg}'),
+          );
+        }
+        else {
+          return const SizedBox.shrink();
+        }
+      }
     );
   }
 }
